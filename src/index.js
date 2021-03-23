54b68dc687f820720aac455379c68b037b61412d
Nikita Lutoshkin <n28280202@gmail.com>
2021-03-23 21:09:06 +0300

    added modalFreeVisit and modalCall

--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@ import mainSlider from './modules/mainSlider';
 import servicesSlider from './modules/servisesSlider';
 import gallerySlider from './modules/gallerySlider';
 import modalFreeVisit from './modules/modalFreeVisit';
+import call from './modules/call';
 
 mainSlider();
 servicesSlider();
 gallerySlider();
 modalFreeVisit();
+call();
--- src/modules/modalFreeVisit.js
+++ src/modules/modalFreeVisit.js
@@ -1,12 +1,25 @@
+import animate from './animate';
+
 const modalFreeVisit = () => {
 	const freeVisitForm = document.getElementById('free_visit_form');
 
 	document.addEventListener('click', event => {
 		const target = event.target;
-		console.log('target: ', target);
 
 		if (target.closest('.free-visit')) {
 			freeVisitForm.style.display = 'block';
+			document.querySelector('.head-slider').style.zIndex = -2;
+			document.querySelector('#totop').style.zIndex = -2;
+
+			animate({
+				duration: 500,
+				timing(timeFraction) {
+					return timeFraction;
+				},
+				draw(progress) {
+					freeVisitForm.style.opacity = progress;
+				}
+			});
 		} else if (!target.closest('.form-content')) {
 			freeVisitForm.style.display = 'none';
 		}
